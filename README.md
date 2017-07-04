653f002069ef71ab599c6ff8964acc96a2654872
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-07-04 15:42:40 +0200

    initial commit with NASA Exoplanet Archive Bulk Downloader

---
+++ README.md
@@ -0,0 +1,46 @@
+# dtools
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Tools for downloading and preparing datasets.
+
+## Usage
+
+* Install [Node.py] with `pip3 install node.py`
+* Install dependencies with `nppm3 install` from inside the dtools directory
+* Run any script as `nodepy3 <path/to/script>`
+
+  [Node.py]: https://nodepy.org/
+
+## NASA Exoplanet Archive
+
+The [NASA Exoplanet Archive][0] offers bulk data downloads which actually only
+provides scripts for the bulk-download that in turn use `wget`. If you don't
+have `wget` installed or you simply want to extract all URLs instead of
+downloading them, this is where *dtools* comes in handy.
+
+For example, you can download the KELT timeseries data from [here][1], extract
+the `.bat` files and pass them to the script to extract the download URLs, or
+download them immediately.
+
+__Extract URLs__
+
+    niklas@lx1 ~/repos/dtools
+    $ nodepy3 nasa/exoplanetarchive extract-urls KELT_N*_wget.bat
+    http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//KELT2/005/055/28/KELT_N02_lc_000001_V01_east_raw_lc.tbl
+    http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//KELT2/005/055/73/KELT_N02_lc_000001_V01_east_tfa_lc.tbl
+    http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//KELT2/005/055/28/KELT_N02_lc_007676_V01_west_raw_lc.tbl
+    ...
+
+__Execute Bulk Download__
+
+    niklas@lx1 ~/repos/dtools
+    $ nodepy3 nasa/exoplanetarchive bulk-download KELT_N*_wget.bat \
+        --to ~/Desktop/KELT
+    [INFO - 2017-07-04 15:33:57,912]: Downloading "KELT_N02_lc_000001_V01_east_raw_lc.tbl" ...
+    [INFO - 2017-07-04 15:34:00,467]: Downloading "KELT_N02_lc_000001_V01_east_tfa_lc.tbl" ...
+    [INFO - 2017-07-04 15:34:02,585]: Downloading "KELT_N02_lc_007676_V01_west_raw_lc.tbl" ...
+    ...
+
+  [0]: https://exoplanetarchive.ipac.caltech.edu/bulk_data_download/
+  [1]: https://exoplanetarchive.ipac.caltech.edu/bulk_data_download/KELT_wget.tar.gz
---
+++ nasa/exoplanetarchive.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2017  Niklas Rosenstein
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+"""
+
+from urllib.parse import urlparse
+import argparse
+import click
+import collections
+import csv
+import logging
+import os
+import posixpath
+import re
+import requests
+import shlex
+import sys
+
+wget_parser = argparse.ArgumentParser()
+wget_parser.add_argument('-O')
+wget_parser.add_argument('-a')
+wget_parser.add_argument('url')
+
+WgetCommand = collections.namedtuple('WgetCommand', 'url ofile logfile')
+logger = logging.getLogger(__name__)
+
+
+def parse_batch_file(filename):
+  printed_warning = False
+  with open(filename) as fp:
+    for line in fp:
+      line = line.strip()
+      if not line or line.startswith('#'): continue
+      if not line.startswith('wget'):
+        if not printed_warning:
+          logger.warn('file "%s" contains commands that are not "wget".', filename)
+          printed_warning = True
+        continue
+      # TODO: Catch exception on parse error.
+      args = wget_parser.parse_args(shlex.split(line)[1:])
+      yield WgetCommand(args.url, args.O, args.a)
+
+
+@click.group()
+def main():
+  logging.basicConfig(level=logging.INFO, format='[%(levelname)s - %(asctime)s]: %(message)s')
+
+
+@main.command('extract-urls')
+@click.argument('files', nargs=-1)
+@click.option('-F', '--format', type=click.Choice(['url', 'csv']),
+  help='The output format.'
+)
+@click.pass_context
+def extract_urls(ctx, files, format):
+  """
+  Extract all URLs from NASA-ExAr .bat files.
+
+  One or more filename must be specified. The files must be .bat files
+  downloaded from the NASA Exoplanet Archive Bulk Download website that
+  contain 'wget' commands for the file download.
+
+  If the output format 'csv' is selected, the output CSV data does not
+  have a header row and its columns are: url|ofile|logfile.
+  """
+
+  if not files:
+    ctx.fail('no input files')
+  if format == 'csv':
+    writer = csv.writer(sys.stdout)
+
+  for filename in files:
+    for wget in parse_batch_file(filename):
+      if format == 'csv':
+        writer.writerow(wget)
+      else:
+        print(wget.url)
+
+
+@main.command('bulk-download')
+@click.argument('files', nargs=-1)
+@click.option('--to', help='The output directory.')
+@click.pass_context
+def bulk_download(ctx, files, to):
+  """
+  Execute the bulk download from NASA-ExAr .bat files without using 'wget'.
+  """
+
+  if not files:
+    ctx.fail('no input files')
+
+  if to and not os.path.isdir(to):
+    os.makedirs(to)
+
+  for filename in files:
+    for wget in parse_batch_file(filename):
+      output_file = wget.ofile
+      if not output_file:
+        output_file = posixpath.basename(urlparse(wget.url).path)
+      logger.info('Downloading "%s" ...', output_file)
+      if to:
+        output_file = os.path.join(to, output_file)
+      response = requests.get(wget.url)
+      try:
+        response.raise_for_status()
+      except Exception as exc:
+        print('  error:', exc)
+        continue
+      with open(output_file, 'wb') as fp:
+        for chunk in response.iter_content(chunk_size=1024):
+          fp.write(chunk)
+
+
+if require.main == module:
+  main()
